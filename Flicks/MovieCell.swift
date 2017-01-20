b56adc1956990d49232ab04387bba5331ef63ec4
Jiapei Liang <liang98@purdue.edu>
2017-01-20 13:11:03 -0500

    Finish two of required user stories

---
+++ Flicks/MovieCell.swift
@@ -0,0 +1,30 @@
+//
+//  MovieCell.swift
+//  Flicks
+//
+//  Created by Jiapei Liang on 1/20/17.
+//  Copyright © 2017 jiapei. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var overviewLabel: UILabel!
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
